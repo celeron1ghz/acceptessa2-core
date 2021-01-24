77234cc151dfab23ca0ca48d928ae766af85cfc1
celeron1ghz <celeron1ghz@gmail.com>
2021-01-24 19:04:55 +0900

    fix test

--- mail/src/lib/Acceptessa2/Mail.pm
+++ mail/src/lib/Acceptessa2/Mail.pm
@@ -2,8 +2,10 @@ package Acceptessa2::Mail;
 use strict;
 use warnings;
 
+use Acceptessa2::Mail::Parameter;
+
 use Try::Tiny;
-use Scalar::Util 'reftype';
+use Encode;
 
 use Paws;
 use Text::Xslate;
@@ -12,43 +14,36 @@ use Email::MIME;
 my $TEMPLATE_BUCKET   = 'acceptessa2-mail-template';
 my $ATTACHMENT_BUCKET = 'acceptessa2-mail-attachment';
 
-sub run {
-    my ( $class, $payload ) = @_;
-
-    ## param check
-    if ( ( reftype($payload) || '' ) ne 'HASH' ) {
-        return { error => 'invalid parameter' };
-    }
-
-    my $template = $payload->{template};
-    my $data     = $payload->{data};
-
-    if ( !$template || reftype($template) ) {
-        return { error => 'template is invalid' };
-    }
-
-    if ( ( reftype($data) || '' ) ne 'HASH' ) {
-        return { error => 'data is invalid' };
-    }
-
-    my $s3 = Paws->service( 'S3', region => 'ap-northeast-1' );
-    my $object;
-    my @body;
-
-    ## fetch template
+sub get_template {
+    my ($self, $key) = @_;
+    my $s3  = Paws->service('S3', region => 'ap-northeast-1');
     my $ret = try {
-        $object = $s3->GetObject( Bucket => $TEMPLATE_BUCKET, Key => $template );
-        return;
+        return $s3->GetObject(Bucket => $TEMPLATE_BUCKET, Key => $key);
     }
     catch {
-        return { error => sprintf "error on get template: %s %s", $_->http_status, $_->code };
+        warn "template not found: $key";
+        return;
     };
+}
 
-    return $ret if $ret;
+sub send_mail {
+    my $ses = Paws->service('SES', region => 'us-east-1');
+
+}
+
+sub run {
+    my ($class, $payload) = @_;
+    my $p = try { Acceptessa2::Mail::Parameter->new($payload) }
+      or return { error => 'invalid parameter' };
+
+    ## fetch template
+    my $tmpl = $class->get_template($p->template)
+      or return { error => 'template not found' };
 
     ## render template
     my $tx       = Text::Xslate->new();
-    my $rendered = $tx->render_string( $object->Body, $data );
+    my $rendered = $tx->render_string($tmpl, $p->data);
+    my @body;
 
     push @body,
       Email::MIME->create(
@@ -57,22 +52,23 @@ sub run {
             'charset'      => 'utf-8',
             'encoding'     => 'base64',
         },
-        'body' => encode( 'utf-8', $rendered ),
+        'body' => encode('utf-8', $rendered),
       );
 
     my $parent = Email::MIME->create(
         header => [
-            'From'    => encode( 'MIME-Header-ISO_2022_JP', sprintf "%s <%s>", $ex->exhibition_name, $from ),
-            'To'      => encode( 'MIME-Header-ISO_2022_JP', $to ),
-            'Subject' => encode( 'MIME-Header-ISO_2022_JP', $subject ),
-            $cc ? ( 'Cc' => $cc ) : (),
+            'From'    => encode('MIME-Header-ISO_2022_JP', $p->from),      # sprintf "%s <%s>", $ex->exhibition_name, $from,
+            'To'      => encode('MIME-Header-ISO_2022_JP', $p->to),
+            'Subject' => encode('MIME-Header-ISO_2022_JP', $p->subject),
+            $p->cc ? ('Cc' => $p->cc) : (),
         ],
         parts => \@body,
     );
 
     ## send mail
+    $class->send_mail($parent->as_string);
 
-    return 1;
+    return { success => 1 };
 }
 
 1;
---
+++ mail/src/t/01-param-check.t
@@ -0,0 +1,57 @@
+use strict;
+use Test::More;
+use Test::LongString;
+use Acceptessa2::Mail;
+use YAML;
+
+sub cr2crlf {
+    my $val = shift;
+    return join("\r\n", split "\n", $val) . "\r\n";
+}
+
+my $data  = YAML::Load(join "", <DATA>);
+my @tests = (
+    {
+        template  => $data->{tmpl1},
+        expected  => cr2crlf($data->{result1}),
+        desc      => 'basic template',
+        parameter => {
+            from     => 'from@from',
+            to       => 'to@to',
+            subject  => 'subject subject',
+            template => "z",
+            data     => {
+                hello => "mogemoge",
+                world => "fugafuga",
+            }
+        },
+        ret => { success => 1 },
+    },
+);
+
+plan tests => @tests * 2;
+local *Email::Simple::Creator::_date_header = sub { 'ThisIsDateString' };
+
+foreach my $t (@tests) {
+    my $result;
+    local *Acceptessa2::Mail::get_template = sub { $t->{template} };
+    local *Acceptessa2::Mail::send_mail    = sub { $result = $_[1] };
+
+    my $ret = Acceptessa2::Mail->run($t->{parameter});
+    is_deeply $ret, $t->{ret}, "$t->{desc}: return value ok";
+    is_string $t->{expected}, $result, "$t->{desc}:   template render ok";
+}
+
+__DATA__
+tmpl1: |
+  <: $hello :> <: $world :>
+result1: |
+  From: from@from
+  To: to@to
+  Subject: subject subject
+  Date: ThisIsDateString
+  MIME-Version: 1.0
+  Content-Transfer-Encoding: base64
+  Content-Type: text/html; charset=utf-8
+  
+  bW9nZW1vZ2UgZnVnYWZ1Z2EK
