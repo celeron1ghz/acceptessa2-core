d139a31b2b654221b052d6fd205c47c65a99fc6e
celeron1ghz <celeron1ghz@gmail.com>
2021-03-10 21:36:18 +0900

    refactored

---
+++ global/tf/11-log-aggregate.tf
@@ -0,0 +1,48 @@
+data "aws_iam_policy_document" "assume-log-aggregate" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.ap-northeast-1.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "policy-log-aggregate" {
+  statement {
+    sid = "1"
+    actions = [
+      "firehose:PutRecord",
+      "firehose:PutRecordBatch"
+    ]
+
+    resources = [
+      aws_kinesis_firehose_delivery_stream.log.arn
+    ]
+  }
+}
+
+resource "aws_iam_role" "log-aggregate" {
+  name               = "${local.appid}-log-aggregate"
+  assume_role_policy = data.aws_iam_policy_document.assume-log-aggregate.json
+}
+
+resource "aws_iam_policy" "log-aggregate" {
+  name   = "${local.appid}-log-aggregate"
+  policy = data.aws_iam_policy_document.policy-log-aggregate.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach-log-aggregate" {
+  role       = aws_iam_role.log-aggregate.name
+  policy_arn = aws_iam_policy.log-aggregate.arn
+}
+
+# resource "aws_cloudwatch_log_subscription_filter" "test_lambdafunction_logfilter" {
+#   name            = "test111111"
+#   role_arn        = aws_iam_role.log-aggregate.arn
+#   log_group_name  = "/aws/lambda/gomitter-dev-main"
+#   filter_pattern  = ""
+#   destination_arn = aws_kinesis_firehose_delivery_stream.log.arn
+#   distribution    = "Random"
+# }
