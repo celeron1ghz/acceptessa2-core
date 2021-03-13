15632bdf8937d487d84e52ad8b5eee4c3654c8dd
celeron1ghz <celeron1ghz@gmail.com>
2021-03-13 15:29:35 +0900

    add lamnda function impl

--- tf/03-kinesis-capture.tf
+++ tf/03-kinesis-capture.tf
@@ -44,6 +44,20 @@ data "aws_iam_policy_document" "policy-cloudwatch" {
       module.log-database.kinesis_arn
     ]
   }
+
+  statement {
+    sid = "3"
+    actions = [
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:DescribeStream",
+      "dynamodb:ListStreams"
+    ]
+
+    resources = [
+      aws_dynamodb_table.token.stream_arn
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "lambda_access_policy" {
@@ -65,10 +79,10 @@ resource "aws_lambda_function" "dynamodb-capture" {
   timeout          = 10
   role             = aws_iam_role.lambda_iam_role.arn
   source_code_hash = data.archive_file.function.output_base64sha256
+}
 
-  lifecycle {
-    ignore_changes = [
-      source_code_hash
-    ]
-  }
+resource "aws_lambda_event_source_mapping" "example" {
+  event_source_arn  = aws_dynamodb_table.token.stream_arn
+  function_name     = aws_lambda_function.dynamodb-capture.arn
+  starting_position = "LATEST"
 }
--- tf/lambda/handler.js
+++ tf/lambda/handler.js
@@ -1,4 +1,50 @@
-module.exports.main = function () {
-    console.log("HelloHello");
-    return 1;
+'use strict';
+
+const aws = require('aws-sdk');
+const firehose = new aws.Firehose();
+
+function flatten(data) {
+  if (!data) {
+    return null;
+  }
+
+  const ret = {};
+
+  try {
+    for (const [key, val] of Object.entries(data)) {
+      ret[key] = Object.entries(val)[0][1];
+    }
+
+  } catch (error) {
+    console.log("error on extracting data: ", error);
+  }
+
+  return ret;
 }
+
+module.exports.main = async (event) => {
+  const result = [];
+
+  for (const r of event.Records) {
+    //console.log(JSON.stringify(r.dynamodb));
+    const splitted = r['eventSourceARN'].split("/");
+    const epoch = r.dynamodb.ApproximateCreationDateTime;
+
+    const row = {
+      table: splitted[1],
+      action: r['eventName'],
+      newImage: flatten(r.dynamodb.NewImage),
+      oldImage: flatten(r.dynamodb.OldImage),
+      userIdentity: r.userIdentity,
+      timestampEpoch: epoch,
+      timestampUtc: new Date(epoch * 1000).toISOString(),
+    };
+
+    result.push({ Data: JSON.stringify(row) + "\n" });
+  }
+
+  //console.log(JSON.stringify(result));
+  await firehose.putRecordBatch({ DeliveryStreamName: "acceptessa2-log-database", Records: result }).promise();
+
+  return "OK";
+};
