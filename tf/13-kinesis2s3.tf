2918ad33a48da41284bb29cdb994f14f7066def8
celeron1ghz <celeron1ghz@gmail.com>
2021-03-13 12:20:22 +0900

    add kinesis

--- tf/13-kinesis2s3.tf
+++ tf/13-kinesis2s3.tf
@@ -2,3 +2,13 @@ module "log-database" {
   source = "./module/kinesis_firehose"
   name   = "acceptessa2-log-database"
 }
+
+module "log-application" {
+  source = "./module/kinesis_firehose"
+  name   = "acceptessa2-log-application"
+}
+
+module "log-mail" {
+  source = "./module/kinesis_firehose"
+  name   = "acceptessa2-log-mail"
+}
